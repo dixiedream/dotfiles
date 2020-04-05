764d5f930c11551dbfe5a589d72c9d0b00386031
Alessandro <dixiedream@hotmail.it>
2020-04-05 14:26:12 +0200

    Satisfied

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -82,27 +82,8 @@ awful.util.terminal = terminal
 awful.util.tagnames = {"1", "2", "3", "4", "5"}
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.floating
-    --awful.layout.suit.tile.left,
-    --awful.layout.suit.tile.bottom,
-    --awful.layout.suit.tile.top,
-    --awful.layout.suit.fair,
-    --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
-    --awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max,
-    --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
-    --awful.layout.suit.corner.nw,
-    --awful.layout.suit.corner.ne,
-    --awful.layout.suit.corner.sw,
-    --awful.layout.suit.corner.se,
-    --lain.layout.cascade,
-    --lain.layout.cascade.tile,
-    --lain.layout.centerwork,
-    --lain.layout.centerwork.horizontal,
-    --lain.layout.termfair,
-    --lain.layout.termfair.center,
+    awful.layout.suit.floating,
+    lain.layout.centerwork
 }
 
 awful.util.taglist_buttons =
@@ -112,39 +93,6 @@ awful.util.taglist_buttons =
         1,
         function(t)
             t:view_only()
-        end
-    ),
-    awful.button(
-        {modkey},
-        1,
-        function(t)
-            if client.focus then
-                client.focus:move_to_tag(t)
-            end
-        end
-    ),
-    awful.button({}, 3, awful.tag.viewtoggle),
-    awful.button(
-        {modkey},
-        3,
-        function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ),
-    awful.button(
-        {},
-        4,
-        function(t)
-            awful.tag.viewnext(t.screen)
-        end
-    ),
-    awful.button(
-        {},
-        5,
-        function(t)
-            awful.tag.viewprev(t.screen)
         end
     )
 )
@@ -166,55 +114,8 @@ awful.util.tasklist_buttons =
                 c:raise()
             end
         end
-    ),
-    awful.button(
-        {},
-        2,
-        function(c)
-            c:kill()
-        end
-    ),
-    awful.button(
-        {},
-        3,
-        function()
-            local instance = nil
-
-            return function()
-                if instance and instance.wibox.visible then
-                    instance:hide()
-                    instance = nil
-                else
-                    instance = awful.menu.clients({theme = {width = dpi(250)}})
-                end
-            end
-        end
-    ),
-    awful.button(
-        {},
-        4,
-        function()
-            awful.client.focus.byidx(1)
-        end
-    ),
-    awful.button(
-        {},
-        5,
-        function()
-            awful.client.focus.byidx(-1)
-        end
     )
 )
-
--- lain.layout.termfair.nmaster           = 3
--- lain.layout.termfair.ncol              = 1
--- lain.layout.termfair.center.nmaster    = 3
--- lain.layout.termfair.center.ncol       = 1
--- lain.layout.cascade.tile.offset_x      = dpi(2)
--- lain.layout.cascade.tile.offset_y      = dpi(32)
--- lain.layout.cascade.tile.extra_padding = dpi(5)
--- lain.layout.cascade.tile.nmaster       = 5
--- lain.layout.cascade.tile.ncol          = 2
 
 beautiful.init(string.format("%s/.config/awesome/theme.lua", os.getenv("HOME")))
 -- }}}
@@ -224,9 +125,7 @@ beautiful.init(string.format("%s/.config/awesome/theme.lua", os.getenv("HOME")))
 screen.connect_signal(
     "property::geometry",
     function(s)
-        os.execute(
-            string.format("setbg %s", os.getenv("WALLPAPER_DIR"))
-        )
+        os.execute("setbg")
     end
 )
 --
