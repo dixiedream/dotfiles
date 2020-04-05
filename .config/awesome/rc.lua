6e3bf3733be1adb3e0fcb7eb7975794e6f9f20a7
Alessandro <dixiedream@hotmail.it>
2020-04-05 14:28:25 +0200

    Removed conflicting shortcut

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -189,22 +189,6 @@ globalkeys =
     ),
     -- Default client focus
     awful.key(
-        {altkey},
-        "j",
-        function()
-            awful.client.focus.byidx(1)
-        end,
-        {description = "focus next by index", group = "client"}
-    ),
-    awful.key(
-        {altkey},
-        "k",
-        function()
-            awful.client.focus.byidx(-1)
-        end,
-        {description = "focus previous by index", group = "client"}
-    ),
-    awful.key(
         {modkey, "Control"},
         "j",
         function()
