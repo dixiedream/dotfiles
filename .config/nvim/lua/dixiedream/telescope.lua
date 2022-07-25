0abe644c15506b6f54eba8763f6247a8f3ffad71
Alessandro <dixiedream88@gmail.com>
2022-07-25 14:59:46 +0200

    New telescope remap

--- .config/nvim/lua/dixiedream/telescope.lua
+++ .config/nvim/lua/dixiedream/telescope.lua
@@ -8,13 +8,9 @@ telescope.setup {
 telescope.load_extension('fzf')
 
 local builtin = require("telescope.builtin")
-local project_files = function()
-    local opts = {}
-    local ok = pcall(builtin.git_files, opts)
-    if not ok then builtin.find_files(opts) end
-end
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<Leader>ps', function() builtin.live_grep() end, opts)
+vim.keymap.set('n', '<Leader>pf', function() builtin.find_files() end, opts)
 vim.keymap.set('n', '<Leader>E', function() builtin.diagnostics() end, opts)
-vim.keymap.set('n', '<C-p>', project_files, opts)
+vim.keymap.set('n', '<C-p>', function() builtin.git_files() end, opts)
