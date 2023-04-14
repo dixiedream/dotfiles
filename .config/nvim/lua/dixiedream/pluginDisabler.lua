fcea66de6c34d21a9f4742e73286572e2cc6361b
Alessandro <dixiedream@hotmail.it>
2023-04-14 18:45:35 +0200

    removed php from treesitter and removed unused nvim providers

--- .config/nvim/lua/dixiedream/pluginDisabler.lua
+++ .config/nvim/lua/dixiedream/pluginDisabler.lua
@@ -13,6 +13,10 @@ g.loaded_vimballPlugin = 1
 g.loaded_2html_plugin = 1
 
 g.loaded_matchit = 1
--- g.loaded_matchparen = 1
 g.loaded_logiPat = 1
 g.loaded_rrhelper = 1
+
+-- Disable unused providers
+g.loaded_python3_provider = 0
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
