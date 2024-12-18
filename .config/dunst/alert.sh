8a39521a499e0f0c7d29966ff07a8f972aa1de60
Alessandro <dixiedream@hotmail.it>
2024-12-19 00:28:55 +0100

    go
    increased alacritty size
    
    add ghostty config
    
    ghostty decent conf
    
    sudo ask password fix
    
    removed ligatures
    
    finally a good cursor
    
    make ghostty update itself
    
    add brew to archupdate script
    
    taken from feature/wayland
    
    lf mac os optimization
    
    removed alacritty config
    
    fix new tokyonight theme format
    
    aerospace config
    
    removed x11 unuseful stuff
    
    not needing scripts
    
    using correct aerospace config file name

--- .config/dunst/alert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-isPaused=$(dunstctl is-paused)
-stackTag=$DUNST_STACK_TAG
-
-[ "$isPaused" = "false" ] && \
-  [ "$stackTag" != "low-battery" ] && \
-    paplay ~/.config/dunst/alert.wav
